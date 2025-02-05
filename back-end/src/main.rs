19649ef5763733f7824e2b6719b6d78416dfe7dc
kelvintanita <kelvintanita@gmail.com>
2025-02-05 00:53:17 -0300

    feat: add cors para qualquer origem

--- back-end/src/main.rs
+++ back-end/src/main.rs
@@ -1,6 +1,7 @@
-use std::io;
+use actix_cors::Cors;
+use actix_web::{http, App, HttpServer};
 use std::env;
-use actix_web::{App, HttpServer};
+use std::io;
 use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
 
@@ -19,18 +20,28 @@ async fn main() -> io::Result<()> {
     env_logger::init();
     dotenv().ok();
 
-    let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());  // Lendo porta do ambiente
+    let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string()); // Lendo porta do ambiente
     let port: u16 = port.parse().expect("PORT inválida");
 
     info!("Servidor iniciando na porta {}", port);
-    
+
     HttpServer::new(|| {
         App::new()
+            .wrap(
+                Cors::default()
+                    .allow_any_origin()
+                    .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
+                    .allowed_headers(vec![
+                        http::header::CONTENT_TYPE,
+                        http::header::AUTHORIZATION,
+                    ])
+                    .max_age(3600),
+            )
             .service(get_block)
             .service(get_transaction)
             .service(get_balance)
             .service(
-                SwaggerUi::new("/swagger-ui/{_:.*}").url("/api/openapi.json", ApiDoc::openapi()), 
+                SwaggerUi::new("/swagger-ui/{_:.*}").url("/api/openapi.json", ApiDoc::openapi()),
             )
     })
     .bind(("0.0.0.0", port))?
